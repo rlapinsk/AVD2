6dfaccc9f1bfeb166bca19beb10716b49fc528b2
rlapinsk <rafal.lapinski@capgemini.com>
2022-05-26 17:18:45 +0200

    a

--- vnet/main.tf
+++ vnet/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_virtual_network" "myvnet"{
     name                = var.vnet_name
     location            = var.deploy_location
     resource_group_name = var.rg_name
-    adress_space        = [10.0.0.0/16]
+    #adress_space        = [10.0.0.0/16]
 
 }
