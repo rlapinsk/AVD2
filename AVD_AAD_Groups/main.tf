97183930ba9460063f1b2f732d9299980741b24e
rlapinsk <rafal.lapinski@capgemini.com>
2022-06-02 18:08:40 +0200

    com

--- AVD_AAD_Groups/main.tf
+++ AVD_AAD_Groups/main.tf
@@ -15,3 +15,20 @@ resource "azuread_group" "AVD_User" {
   security_enabled = true
   provider = azuread.ad_ten
 }
+
+data "azurerm_resource_group" "primary" {
+  name = var.rg_name
+}
+
+resource "azurerm_role_assignment" "example1" {
+  scope                = data.azurerm_resource_group.primary.id
+  role_definition_name = "Virtual Machine Administrator Login"
+  principal_id         = azuread_group.AVD_Admin.object_id
+}
+
+resource "azurerm_role_assignment" "example2" {
+  scope                = data.azurerm_resource_group.primary.id
+  role_definition_name = "Virtual Machine User Login"
+  principal_id         = azuread_group.AVD_User.object_id
+}
+
--- Service_Principal/main.tf
+++ Service_Principal/main.tf
@@ -10,3 +10,11 @@ resource "azuread_service_principal_password" "avd_spn_pswd" {
   service_principal_id = azuread_service_principal.avd_spn.id
 }
 
+data "azurerm_subscription" "primary" {
+}
+
+resource "azurerm_role_assignment" "example" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.avd_spn.object_id
+}
